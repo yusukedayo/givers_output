7ed705502f55f5319347242cc4827c8fb4cfd090
yusukedayo <tsunemi0217@icloud.com>
2022-05-05 12:38:06 +0900

    Add: ログイン機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'tweets#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'user_sessions#new'
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+  resources :users, only: %i[new create]
 end
